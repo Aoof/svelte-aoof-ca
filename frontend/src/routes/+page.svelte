4d64b1be3e77697b3e9f0ad35ec9d45b76b58655
Abdulrahman M <53632818+Aoof@users.noreply.github.com>
2024-06-29 00:38:53 -0400

    Fixed margin issues

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -45,7 +45,17 @@
 </style>
 
 <svelte:head>
-    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="aoof" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+    <script 
+      data-name="BMC-Widget" 
+      data-cfasync="false" 
+      src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" 
+      data-id="aoof" 
+      data-description="Support me on Buy me a coffee!" 
+      data-message="" 
+      data-color="#FF813F" 
+      data-position="Right" 
+      data-x_margin="18" 
+      data-y_margin="80"></script>
 </svelte:head>
 
 <main>
