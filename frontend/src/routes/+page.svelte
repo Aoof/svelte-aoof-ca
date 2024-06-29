ddae12f68b6d9bd1b6158188e28b1854309e221f
Abdulrahman M <53632818+Aoof@users.noreply.github.com>
2024-06-29 00:33:14 -0400

    Added buymeacoffee only on portfolio page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -44,6 +44,10 @@
   }
 </style>
 
+<svelte:head>
+    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="aoof" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+</svelte:head>
+
 <main>
   <Navbar />
   <div class="content-container">
