98a6c7c5375af11631efec7d10a727920f484d81
aoof <aoofxd@gmail.com>
2025-08-12 02:48:04 -0400

    detachment from old setup

--- frontend/src/routes/fae-sparkles/recipe/[id]/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import { page } from '$app/stores';
-    
-    import SingleRecipe from '$lib/components/Fae/SingleRecipe.svelte';
-</script>
-
-<SingleRecipe id={$page.params.id} />
